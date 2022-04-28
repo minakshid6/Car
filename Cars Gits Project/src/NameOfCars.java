6b77ebe94b36e3ff1ef55ba02296f47d619f2331
minakshid6 <104327664+minakshid6@users.noreply.github.com>
2022-04-28 00:36:43 -0500

    commit

---
+++ Cars Gits Project/src/NameOfCars.java
@@ -0,0 +1,21 @@
+
+public class NameOfCars {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is Audi colour moonlight blue ");
+		System.out.println("this is BMW CLOUR black");
+		System.out.println("this is Tesla electric car");
+		System.out.println("this is volvo xc90 clour black");
+		System.out.println("this is Accura mdx");
+		System.out.println("*****done********");
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
